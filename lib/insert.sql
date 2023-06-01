7065684335f27be3cb76147ed9768e571e4dbe6d
florence wangechi <florencewangechim@gmail.com>
2023-06-01 13:50:02 +0300

    SQL

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr. Chocolate",4,"M","Darkbrown",37,1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",3,"F","BLack",38,0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha",6,"F","White",36,1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",2,"M","brown",39,1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa",7,"F","Black",37,1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch",5,"M","white",33,1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy",7,"F","Brown",37,1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,NULL,"M",NULL,NULL,1);
